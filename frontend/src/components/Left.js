2484a410ac48a33d56a4c75c1c62c382e67e68e8
dlstjq7685 <dlstjq7685@gmail.com>
2018-10-08 12:14:00 +0900

    update

--- frontend/src/components/Left.js
+++ frontend/src/components/Left.js
@@ -2,6 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import brace from 'brace';
+import AceEditor from 'react-ace';
+import 'brace/mode/python';
+import 'brace/theme/monokai';
+import 'brace/ext/language_tools';
+
+
 
 const styles = theme => ({
   test: {
@@ -11,28 +18,44 @@ const styles = theme => ({
 
 class left extends React.Component {
   state = {
-    open: false,
-    anchor: 'left',
+    val: ''
   };
   
   constructor(props){
     super(props)
 
     this.test = props.test
-    this.handleChange = this.handleChange.bind(this);
-
+    this.onChange = this.onChange.bind(this)
+    console.log(window.innerHeight)
+    console.log(window.innerWidth)
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  onChange(newValue) {
+    this.setState({val: newValue})
   }
-
+  
   render() {
 
     return (
       <div>
+        <AceEditor
+      mode="python"
+      theme="monokai"
+      onChange={e => this.onChange}
+      name="UNIQUE_ID_OF_DIV"
+      fontSize={18}
+      height={100}
+      editorProps={{$blockScrolling: false}}
+      setOptions={{
+        enableBasicAutocompletion: false,
+        enableLiveAutocompletion: true,
+        enableSnippets: false,
+        showLineNumbers: true,
+        tabSize: 4,
+        }}      
+      />
+      <Button onClick={e => this.test('test')}>test</Button>
 
-        <Button onClick={e => this.test('test')}>test</Button>
       </div>
     );
   }
