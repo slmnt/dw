302b2eba261fd0496de29f37cf21d8abbc9f5c6b
kanamentosu <kd1251828@st.kobedenshi.ac.jp>
2018-10-31 09:24:21 +0900

    Main.js変更(池田)

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import { instanceOf } from 'prop-types';
 import { withCookies, Cookies } from 'react-cookie';
+
+const styles = theme => ({
+  body: {
+    padding:"10px",
+    backgroundColor: "#ffffff",
+    height:"100vh",
+    width:"100vw"
+  },
+  header: {
+    textAlign:"center",
+  },
+  title: {
+    color: "#99ff33",
+    fontSize: "100px",
+    padding: "50px"
+  },
+  sub_title: {
+    color: "#000000",
+    fontSize: "50px"
+  }
+})
 
 class Main extends Component {
   static propTypes = {
@@ -18,13 +40,26 @@ class Main extends Component {
     this.state = {value: '', color: 'green'};
   }
 
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        wellcome mini program main page
-      </div>
+      <body className = {classes.body}>
+        <header className = {classes.header}>
+          <h1 className = {classes.title}>
+            Mini Prog
+          </h1>
+          <h2 className = {classes.sub_title}>
+            For all people who study programming.
+          </h2> 
+        </header>
+      </body>
+      
+      
+      
     )
   }
 }
-  
-export default withCookies(Main)
+Main = withCookies(Main)  
+export default withStyles(styles, {withTheme: true})(Main)
