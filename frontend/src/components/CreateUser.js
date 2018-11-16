f1ea0a82aefe72d6f05d7552acf4f5212a9fa188
kanamentosu <kd1251828@st.kobedenshi.ac.jp>
2018-11-16 14:49:53 +0900

    fix

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,19 +1,100 @@
-import React, { Component } from 'react';   
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-class CUser extends Component {
+const styles = theme => ({
+  main: {
+    fontFamily:"arial black,Yu Gothic",
+    width: 'auto',
+    display: 'block',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    backgroundColor:"#f2f7ff",
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 500,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 5,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  signup:{
+    color:"white",
+    backgroundColor:"#1abc9c",
+    width:"100%",
+    textAlign:"center",
+    padding:"5px",
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing.unit,
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+    backgroundColor:"#1abc9c",
+  },
+});
 
-    constructor(props){
-      super(props)
+function SignIn(props) {
+  const { classes } = props;
 
-    }
-
-    render() {
-      return (
-        <div>
-            Cuser
-        </div>
-        );
-  	}
+  return (
+    <main className={classes.main}>
+      <CssBaseline />
+      <Paper className={classes.paper}>
+        <Typography component="h2" variant="h5" className={classes.signup}>
+          Mini Prog 会員登録
+        </Typography>
+        <form className={classes.form}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="id">ID(半角英数字)</InputLabel>
+            <Input name="id" type="text" id="id" autoComplete="username" />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">パスワード</InputLabel>
+            <Input name="password" type="password" id="password" autoComplete="new-password" />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">メールアドレス</InputLabel>
+            <Input id="email" name="email" autoComplete="email" autoFocus />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="first">名(例:太郎)</InputLabel>
+            <Input name="first" type="text" id="first" autoComplete="given-name" />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="last">性(例:山田)</InputLabel>
+            <Input name="last" type="text" id="last" autoComplete="family-name" />
+          </FormControl>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            登録
+          </Button>
+        </form>
+      </Paper>
+    </main>
+  );
 }
 
-export default CUser;
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SignIn);
