5f985206e8cbbdbef8d42c87435da1207c1e16e7
slmnt <33847832+slmnt@users.noreply.github.com>
2018-11-26 18:19:45 +0900

    yarn 追加 & game の destroy

--- frontend/src/components/three.js
+++ frontend/src/components/three.js
@@ -17,6 +17,9 @@ class three extends Component {
         Minecraft.init();
         this.update();
     }
+    componentWillUnmount(){
+        Minecraft.destroy();
+    }
     update = () => {
         window.requestAnimationFrame(this.update);
         Minecraft.update();
