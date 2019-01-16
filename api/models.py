271cc258bd3536873fd0e16ee43b58d7a5f1ac2f
dlstjq7685 <dlstjq7685@gmail.com>
2019-01-16 15:57:29 +0900

    api model fix

--- api/models.py
+++ api/models.py
@@ -48,7 +48,7 @@ class Code(models.Model):
         return '%d' % (self.id)
 
 class Comment(models.Model):
-    auth = models.ForeignKey(User,on_delete=False, null=False,default=False)
+    auth = models.ForeignKey(User,on_delete=models.CASCADE, null=False,default=False)
     coments = models.CharField(max_length=1000)
     createat = models.DateTimeField(default=timezone.now)
     root = models.ForeignKey('Code',on_delete=models.CASCADE)
@@ -59,7 +59,7 @@ class Open(models.Model):
 
 class Techinfo(models.Model):
     title = models.CharField(max_length=15)
-    auth = models.ForeignKey(User,on_delete=False, null=False,default=False)
+    auth = models.ForeignKey(User,on_delete=models.CASCADE, null=False,default=False)
     context = models.TextField()
     createat = models.DateTimeField(default=timezone.now)
     count = models.IntegerField(default=0)
@@ -67,7 +67,7 @@ class Techinfo(models.Model):
 
 class Usertechinfo(models.Model):
     title = models.CharField(max_length=15,default='null')
-    auth = models.ForeignKey(User,on_delete=False, null=False,default=False)
+    auth = models.ForeignKey(User,on_delete=models.CASCADE, null=False,default=False)
     context = models.TextField()
     createat = models.DateTimeField(default=timezone.now)
     updateat = models.DateTimeField(default=timezone.now)
@@ -78,18 +78,18 @@ class Usertechinfo(models.Model):
         return '%d' % (self.id)
 
 class Usertechinfocomment(models.Model):
-    auth = models.ForeignKey(User,on_delete=False, null=False,default=False)
+    auth = models.ForeignKey(User,on_delete=models.CASCADE, null=False,default=False)
     coments = models.CharField(max_length=1000)
     createat = models.DateTimeField(default=timezone.now)
     root = models.ForeignKey('Usertechinfo',on_delete=models.CASCADE)
 
 class Ads(models.Model):
-    auth = models.ForeignKey(User,on_delete=False, null=False,default=False)
+    auth = models.ForeignKey(User,on_delete=models.CASCADE, null=False,default=False)
     context = models.TextField()
 
 class Adque(models.Model):
     ads = models.ForeignKey('Ads',on_delete=models.CASCADE)
 
 class CertiList(models.Model):
-    name = models.ForeignKey(User,on_delete=False, null=False,default=False)
+    name = models.ForeignKey(User,on_delete=models.CASCADE, null=False,default=False)
     code = models.CharField(max_length=25)
