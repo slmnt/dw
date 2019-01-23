54ec4c3a5504aeb9db9c321d711cd0ab90da8dca
slmnt <33847832+slmnt@users.noreply.github.com>
2019-01-24 00:48:48 +0900

    chore

--- frontend/src/components/pages/CourseSearch.js
+++ frontend/src/components/pages/CourseSearch.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Loading from '../Loading'
 import './CourseSearch.css'
-import Ad from '../AdPanel'
+import Ad from '../Adpanel'
 import axios from 'axios';
 
 import { Link } from 'react-router-dom';
